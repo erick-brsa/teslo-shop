6f22447b116bc2d954974e3f36b6c3e8d27c7168
Erick Briones <erickalan518@gmail.com>
2022-09-13 13:35:45 -0500

    Guardando productos del carrito en las cookies

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -5,19 +5,24 @@ import { ICartProduct } from '../../interfaces';
 import { CartContext, cartReducer } from './';
 
 export interface CartState {
+	isLoaded: boolean;
 	cart: ICartProduct[];
 	numberOfItems: number;
 	subTotal: number;
 	tax: number;
 	total: number;
+
+	// shippingAdress: ShippingAdress;
 }
 
 const CART_INITIAL_STATE: CartState = {
+	isLoaded: false,
 	cart: [],
 	numberOfItems: 0,
 	subTotal: 0,
 	tax: 0,
-	total: 0
+	total: 0,
+	// shippingAdress: undefined
 };
 
 interface Props {
@@ -25,6 +30,7 @@ interface Props {
 }
 
 export const CartProvider: FC<Props> = ({ children }) => {
+
 	const [state, dispatch] = useReducer(cartReducer, CART_INITIAL_STATE);
 
 	useEffect(() => {
@@ -45,18 +51,14 @@ export const CartProvider: FC<Props> = ({ children }) => {
 	}, []);
 
 	useEffect(() => {
-		Cookie.set('cart', JSON.stringify(state.cart));
+		if (state.cart.length > 0){
+			Cookie.set('cart', JSON.stringify(state.cart));
+		}
 	}, [state.cart]);
 
 	useEffect(() => {
-		const numberOfItems = state.cart.reduce(
-			(prev, current) => current.quantity + prev,
-			0
-		);
-		const subTotal = state.cart.reduce(
-			(prev, current) => current.price * current.quantity + prev,
-			0
-		);
+		const numberOfItems = state.cart.reduce((prev, current) => current.quantity + prev, 0);
+		const subTotal = state.cart.reduce((prev, current) => current.price * current.quantity + prev, 0);
 		const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
 
 		const orderSummary = {
@@ -69,29 +71,28 @@ export const CartProvider: FC<Props> = ({ children }) => {
 		dispatch({
 			type: '[Cart] - Update order summary',
 			payload: orderSummary
-			
 		});
 	}, [state.cart]);
 
 	const addProductToCart = (product: ICartProduct) => {
-		const productInCart = state.cart.some((p) => p._id === product._id);
-		if (!productInCart)
+		const productInCart = state.cart.some(p => p._id === product._id);
+		if (!productInCart) {
 			return dispatch({
 				type: '[Cart] - Update products in cart',
 				payload: [...state.cart, product]
 			});
+		}
 
-		const productInCartButWithDifferentSize = state.cart.some(
-			(p) => p._id === product._id && p.size !== product.size
-		);
-		if (productInCartButWithDifferentSize)
+		const productInCartButWithDifferentSize = state.cart.some(p => p._id === product._id && p.size !== product.size);
+		if (productInCartButWithDifferentSize){
 			return dispatch({
 				type: '[Cart] - Update products in cart',
 				payload: [...state.cart, product]
 			});
-
+		}
+		
 		// Acumular
-		const updatedProducts = state.cart.map((p) => {
+		const updatedProducts = state.cart.map(p => {
 			if (p._id !== product._id) return p;
 			if (p.size !== product.size) return p;
 			// Actualizar la cantidad
@@ -114,6 +115,55 @@ export const CartProvider: FC<Props> = ({ children }) => {
 		dispatch({ type: '[Cart] - Remove product in cart', payload: product });
 	};
 
+	// const updateAddress = ( address: ShippingAddress ) => {
+    //     Cookie.set('firstName',address.firstName);
+    //     Cookie.set('lastName',address.lastName);
+    //     Cookie.set('address',address.address);
+    //     Cookie.set('address2',address.address2 || '');
+    //     Cookie.set('zip',address.zip);
+    //     Cookie.set('city',address.city);
+    //     Cookie.set('country',address.country);
+    //     Cookie.set('phone',address.phone);
+    //     dispatch({ type: '[Cart] - Update Address', payload: address });
+    // }
+
+	// const createOrder = async():Promise<{ hasError: boolean; message: string; }> => {
+    //     if ( !state.shippingAddress ) {
+    //         throw new Error('No hay dirección de entrega');
+    //     }
+    //     const body: IOrder = {
+    //         orderItems: state.cart.map( p => ({
+    //             ...p,
+    //             size: p.size!
+    //         })),
+    //         shippingAddress: state.shippingAddress,
+    //         numberOfItems: state.numberOfItems,
+    //         subTotal: state.subTotal,
+    //         tax: state.tax,
+    //         total: state.total,
+    //         isPaid: false
+    //     }
+    //     try {
+    //         const { data } = await tesloApi.post<IOrder>('/orders', body);
+    //         dispatch({ type: '[Cart] - Order complete' });
+    //         return {
+    //             hasError: false,
+    //             message: data._id!
+    //         }
+    //     } catch (error) {
+    //         if ( axios.isAxiosError(error) ) {
+    //             return {
+    //                 hasError: true,
+    //                 message: error.response?.data.message
+    //             }
+    //         }
+    //         return {
+    //             hasError: true,
+    //             message : 'Error no controlado, hable con el administrador'
+    //         }
+    //     }
+    // }
+
 	return (
 		<CartContext.Provider
 			value={{
@@ -122,9 +172,12 @@ export const CartProvider: FC<Props> = ({ children }) => {
 				// Methods
 				addProductToCart,
 				removeCartProduct,
-				updateCartQuantity
-			}}
-		>
+				updateCartQuantity,
+				// updateAdress,
+
+				// Orders
+				// createOrder
+		}}>
 			{children}
 		</CartContext.Provider>
 	);
