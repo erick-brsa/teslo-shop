6dd99c73fe2d0c2e098eef413358a672469bdb7e
Erick Briones <erickalan518@gmail.com>
2022-10-05 23:51:18 -0500

    Implementando la pasarela de pagos con PayPal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,42 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { SessionProvider } from 'next-auth/react';
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js'
 
 import type { AppProps } from 'next/app';
 import { SWRConfig } from 'swr';
 
 import { CartProvider, UiProvider, AuthProvider } from '../context';
 import { lightTheme } from '../themes';
+
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<SessionProvider>
-			<SWRConfig
-				value={{
-					// refreshInterval: 3000,
-					fetcher: (resource, init) =>
-						fetch(resource, init).then((res) => res.json())
+			<PayPalScriptProvider
+				options={{
+					'client-id': process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || ''
 				}}
 			>
-				<AuthProvider>
-					<CartProvider>
-						<UiProvider>
-							<ThemeProvider theme={lightTheme}>
-								<CssBaseline />
-								<Component {...pageProps} />
-							</ThemeProvider>
-						</UiProvider>
-					</CartProvider>
-				</AuthProvider>
-			</SWRConfig>
+				<SWRConfig
+					value={{
+						// refreshInterval: 3000,
+						fetcher: (resource, init) =>
+							fetch(resource, init).then((res) => res.json())
+					}}
+				>
+					<AuthProvider>
+						<CartProvider>
+							<UiProvider>
+								<ThemeProvider theme={lightTheme}>
+									<CssBaseline />
+									<Component {...pageProps} />
+								</ThemeProvider>
+							</UiProvider>
+						</CartProvider>
+					</AuthProvider>
+				</SWRConfig>
+			</PayPalScriptProvider>
 		</SessionProvider>
 	);
 }
--- pages/orders/[id].tsx
+++ pages/orders/[id].tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import { getSession } from 'next-auth/react';
 import { GetServerSideProps, NextPage } from 'next';
-
+import { PayPalButtons } from '@paypal/react-paypal-js';
+import { useRouter } from 'next/router';
 import NextLink from 'next/link';
+
 import {
 	Typography,
 	Grid,
@@ -9,23 +12,59 @@ import {
 	Divider,
 	Box,
 	Link,
-	Chip
+	Chip,
+	CircularProgress
 } from '@mui/material';
-import { CreditCardOffOutlined, CreditScoreOutlined } from '@mui/icons-material';
+import {
+	CreditCardOffOutlined,
+	CreditScoreOutlined
+} from '@mui/icons-material';
 
 import { CartList, OrderSummary } from '../../components/cart';
 import { ShopLayout } from '../../components/layouts';
 import { IOrder } from '../../interfaces';
 import { dbOrders } from '../../database';
+import { tesloApi } from '../../config';
 
+export type OrderResponseBody = {
+	id: string;
+	status:
+		| 'COMPLETED'
+		| 'SAVED'
+		| 'APPROVED'
+		| 'VOIDED'
+		| 'PAYER_ACTION_REQUIRED';
+};
 interface Props {
 	order: IOrder;
 }
 
 const OrderPage: NextPage<Props> = ({ order }) => {
-
+	const router = useRouter();
 	const { isPaid, shippingAddress, numberOfItems, orderItems } = order;
-	
+	const [isPaying, setIsPaying] = useState(false);
+
+	const onOrderCompleted = async (details: OrderResponseBody) => {
+		if (details.status !== 'COMPLETED') {
+			return alert('No hay pago en PayPal');
+		}
+
+		setIsPaying(true);
+
+		try {
+			const { data } = await tesloApi.post(`/orders/pay`, {
+				transactionId: details.id,
+				orderId: order._id
+			});
+
+			router.reload();
+		} catch (error) {
+			setIsPaying(false);
+			console.log(error);
+			alert('Hubo un error');
+		}
+	};
+
 	return (
 		<ShopLayout
 			title={'Resumen de la orden'}
@@ -35,39 +74,34 @@ const OrderPage: NextPage<Props> = ({ order }) => {
 				Orden: ASDHE
 			</Typography>
 
-			{
-				isPaid ? (
-					<Chip
-						sx={{ my: 2 }}
-						label="La orden ya fue pagada"
-						variant="outlined"
-						color="success"
-						icon={<CreditScoreOutlined />}
-					/>
-				) : (
-					<Chip 
-						sx={{my: 2}}
-						label="Pendiente de pago"
-						variant='outlined'
-						color="error"
-						icon={<CreditCardOffOutlined />}
-					/>
-				)
-			}
+			{isPaid ? (
+				<Chip
+					sx={{ my: 2 }}
+					label="La orden ya fue pagada"
+					variant="outlined"
+					color="success"
+					icon={<CreditScoreOutlined />}
+				/>
+			) : (
+				<Chip
+					sx={{ my: 2 }}
+					label="Pendiente de pago"
+					variant="outlined"
+					color="error"
+					icon={<CreditCardOffOutlined />}
+				/>
+			)}
 
-			<Grid container spacing={4} className='fadeIn'>
-				
+			<Grid container spacing={4} className="fadeIn">
 				<Grid item xs={12} sm={7}>
-					<CartList 
-							products={orderItems}
-							editable={false} 
-					/>
+					<CartList products={orderItems} editable={false} />
 				</Grid>
 
 				<Grid item xs={12} sm={5}>
 					<Card className="summary-card" sx={{ p: 2 }}>
 						<Typography variant="h4" component="h4">
-							Resumen ({ numberOfItems }) { numberOfItems > 1 ? 'productos' : 'producto' }
+							Resumen ({numberOfItems}){' '}
+							{numberOfItems > 1 ? 'productos' : 'producto'}
 						</Typography>
 						<Divider sx={{ my: 1 }} />
 
@@ -81,7 +115,11 @@ const OrderPage: NextPage<Props> = ({ order }) => {
 							Dirección de entrega
 						</Typography>
 						<Typography>{`${shippingAddress.firstName} ${shippingAddress.lastName}`}</Typography>
-						<Typography>{`${shippingAddress.address} ${shippingAddress.address2 ? `, ${shippingAddress.address2}` : '' }`}</Typography>
+						<Typography>{`${shippingAddress.address} ${
+							shippingAddress.address2
+								? `, ${shippingAddress.address2}`
+								: ''
+						}`}</Typography>
 						<Typography>{`${shippingAddress.city}, ${shippingAddress.zip}`}</Typography>
 						<Typography>{shippingAddress.country}</Typography>
 						<Typography>{shippingAddress.phone}</Typography>
@@ -93,18 +131,40 @@ const OrderPage: NextPage<Props> = ({ order }) => {
 								<Link underline="always">Editar</Link>
 							</NextLink>
 						</Box> */}
-						
-						<OrderSummary orderValues={{
+
+						<OrderSummary
+							orderValues={{
 								numberOfItems: order.numberOfItems,
 								subTotal: order.subTotal,
 								total: order.total,
-								tax: order.tax	
-						}} />
+								tax: order.tax
+							}}
+						/>
 
-						<Box sx={{ mt: 3 }} display="flex" flexDirection="column">
-							{/* TODO: Pagar */}
-							{
-								isPaid ? (
+						<Box
+							sx={{ mt: 3 }}
+							display="flex"
+							flexDirection="column"
+						>
+							<Box
+								display="flex"
+								justifyContent="center"
+								className="fadeIn"
+								sx={{
+									display: isPaying ? 'flex' : 'none'
+								}}
+							>
+								<CircularProgress />
+							</Box>
+
+							<Box 
+								flexDirection="column"
+								sx={{
+									display: isPaid ? 'none' : 'flex',
+									flex: 1
+								}}
+							>
+								{isPaid ? (
 									<Chip
 										sx={{ my: 2 }}
 										label="La orden ya fue pagada"
@@ -112,11 +172,33 @@ const OrderPage: NextPage<Props> = ({ order }) => {
 										color="success"
 										icon={<CreditScoreOutlined />}
 									/>
-								)
-								: (
-									<h3>Pagar</h3>
-								)
-							}
+								) : (
+									<PayPalButtons
+										createOrder={(data, actions) => {
+											return actions.order.create({
+												purchase_units: [
+													{
+														amount: {
+															value: order.total.toString()
+														}
+													}
+												]
+											});
+										}}
+										onApprove={(data, actions) => {
+											return actions
+												.order!.capture()
+												.then(
+													(
+														details: OrderResponseBody
+													) => {
+														onOrderCompleted(details);
+													}
+												);
+										}}
+									/>
+								)}
+							</Box>
 						</Box>
 					</Card>
 				</Grid>
@@ -125,10 +207,12 @@ const OrderPage: NextPage<Props> = ({ order }) => {
 	);
 };
 
-export const getServerSideProps: GetServerSideProps = async ({ req, query }) => {
-
+export const getServerSideProps: GetServerSideProps = async ({
+	req,
+	query
+}) => {
 	const { id = '' } = query;
-	const session: any = await getSession({ req })
+	const session: any = await getSession({ req });
 
 	if (!session) {
 		return {
@@ -136,34 +220,34 @@ export const getServerSideProps: GetServerSideProps = async ({ req, query }) => 
 				destination: `/auth/login?/orders/${id}`,
 				permanent: false
 			}
-		}
+		};
 	}
-	
+
 	const order = await dbOrders.getOrderById(id.toString());
-	
+
 	if (!order) {
 		return {
 			redirect: {
 				destination: `/orders/history`,
 				permanent: false
 			}
-		}
+		};
 	}
-	
+
 	if (order.user !== session.user._id) {
 		return {
 			redirect: {
 				destination: `/orders/history`,
 				permanent: false
 			}
-		}	
+		};
 	}
 
 	return {
 		props: {
 			order
 		}
-	}
-}
+	};
+};
 
 export default OrderPage;
